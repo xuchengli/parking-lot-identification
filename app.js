9dfa89a07e6462f30d297b306b121449e1bc6d5e
lixucheng <lixucheng@aliyun.com>
2017-07-10 17:56:16 +0800

    init i18n support

--- app.js
+++ app.js
@@ -5,12 +5,19 @@ var express = require("express");
 var path = require("path");
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
+var i18n = require("i18n");
 var config = require("./modules/configuration");
+
+i18n.configure({
+    locales: ["en", "zh_CN"],
+    directory: path.join(__dirname, "locales")
+});
 var app = express();
 var env = process.env.NODE_ENV || "production";
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
+app.use(i18n.init);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
