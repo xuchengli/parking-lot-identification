2ce4d2edd2fc1da4c0990c72327807765e0528d5
lixucheng <lixucheng@aliyun.com>
2017-07-11 14:15:04 +0800

    add locale cookie for i18n

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
  */
 var express = require("express");
 var path = require("path");
+var cookieParser = require("cookie-parser");
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 var i18n = require("i18n");
@@ -10,13 +11,16 @@ var config = require("./modules/configuration");
 
 i18n.configure({
     locales: ["en", "zh_CN"],
-    directory: path.join(__dirname, "locales")
+    directory: path.join(__dirname, "locales"),
+    defaultLocale: "zh_CN",
+    cookie: "locale"
 });
 var app = express();
 var env = process.env.NODE_ENV || "production";
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
+app.use(cookieParser());
 app.use(i18n.init);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
