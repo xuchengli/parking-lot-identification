b07665921bf50342f92967670bb827734001d215
lixucheng <lixucheng@aliyun.com>
2017-07-14 18:02:56 +0800

    add iframe to link emap for parking lots edit

--- web/js/index.js
+++ web/js/index.js
@@ -14,22 +14,38 @@ import ParkingLotsMapping from "./parking-lots-mapping";
 UIkit.use(Icons);
 
 var $homeSection = $("#home-section");
+var $osmSection = $("#osm-section");
+var $osmIframe = $("#osm-section iframe");
+var $streetViewSection = $("#street-view-section");
+var $streetViewIframe = $("#street-view-section iframe");
 var $mappingSection = $("#mapping-section");
 var parkingLotsMapping = new ParkingLotsMapping();
 
 var i18n = new I18n();
 i18n.init();
 
+function showSection(name) {
+    name == "home" ? $homeSection.show() : $homeSection.hide();
+    name == "osm" ? $osmSection.show() : $osmSection.hide();
+    name == "streetView" ? $streetViewSection.show() : $streetViewSection.hide();
+    name == "mapping" ? $mappingSection.show() : $mappingSection.hide();
+}
 $.address.change(evt => {
     switch (evt.value) {
         case "/":
-            $homeSection.show();
-            $mappingSection.hide();
+            showSection("home");
             break;
-        case "/dashboard":
+        case "/osm":
+            $osmIframe.attr("src", "https://emap.crl.ibm.com/imd/workbench?app=parking-lots-editor&key=map");
+            showSection("osm");
+            break;
+        case "/street-view":
+            $streetViewIframe.attr("src", "https://emap.crl.ibm.com/imd/workbench?app=parking-lots-editor&key=car");
+            showSection("streetView");
+            break;
+        case "/mapping":
             parkingLotsMapping.load("20f43f02-5bdf-4e51-b5bc-e34dad373bc8");
-            $homeSection.hide();
-            $mappingSection.show();
+            showSection("mapping");
             break;
     }
 });
@@ -48,11 +64,13 @@ $entryCard.hover(evt => {
     var $this = $(evt.currentTarget);
     switch ($this.index(".entry-card")) {
         case 0:
+            $.address.value("/osm");
             break;
         case 1:
+            $.address.value("/street-view");
             break;
         case 2:
-            $.address.value("/dashboard");
+            $.address.value("/mapping");
             break;
     }
 });
